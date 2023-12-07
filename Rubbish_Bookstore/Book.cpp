3329f339753ab3f3ca24e964e279fbeb0c925b38
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-07 19:11:09 +0800

    docs:I find that I'm a 丈育.I mistook fit for fix...

--- Rubbish_Bookstore/Book.cpp
+++ Rubbish_Bookstore/Book.cpp
@@ -1,9 +1,2 @@
 #include "Book.hpp"
 using std::cin;
-void Book::Initial() {
-  cin >> ISBN;
-  cin >> name;
-  cin >> author;
-  cin >> keyword;
-  cin >> in_price;
-}
--- Rubbish_Bookstore/Book.hpp
+++ Rubbish_Bookstore/Book.hpp
@@ -1,17 +1,20 @@
 #include<bits/stdc++.h>
+#include"Store.hpp"
 #ifndef BOOKDATA_HPP
 #define BOOKDATA_HPP
 class Book {
-  protected:
-    char ISBN[21];
-    char name[61];
-    char author[61];
-    char keyword[61];
-    int block_nxt;
-    double price;
-    double in_price;
-  public:
-  void Print();
-  void Initial();
+protected:
+  char ISBN[21];
+  char name[61];
+  char author[61];
+  char keyword[61];
+  double price;
+  int quantity;
+public:
+  Book();
+  void NewBook(char *);
+  void Modify(char *);
+  void Import(char *);
+  void Show();
 };
 #endif
