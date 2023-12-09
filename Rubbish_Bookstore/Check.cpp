4199a0b2b486dec772a23bd2cc93a503bbbf06f5
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-09 09:41:50 +0800

    fix:fix the function checkvisiblenoquotation.

--- Rubbish_Bookstore/Check.cpp
+++ Rubbish_Bookstore/Check.cpp
@@ -27,28 +27,28 @@ void CheckNLU(char *str) {
 }
 
 void CheckEmpty(std::string txt) {
-  if(!txt.empty()) {
+  if (!txt.empty()) {
     throw(0);
   }
   return;
 }
 
 void CheckSize30(char *str) {
-  if(strlen(str) > 30) {
+  if (strlen(str) > 30) {
     throw(0);
   }
   return;
 }
 
 void CheckSize60(char *str) {
-  if(strlen(str) > 60) {
+  if (strlen(str) > 60) {
     throw(0);
   }
   return;
 }
 
 void CheckSize20(char *str) {
-  if(strlen(str) > 20) {
+  if (strlen(str) > 20) {
     throw(0);
   }
   return;
@@ -68,22 +68,22 @@ void CheckVisibleNoQuotation(char *str) {
   int len = strlen(str);
   for (int i = 0; i < len; ++i) {
     if (str[i] <= 32 || str[i] > 126) {
-      if(str[i] == '\"') {
-        throw(0);
-      }
+      throw(0);
     }
+    if (str[i] == '\"') {
+      throw(0);
+    }
+    return;
   }
-  return;
 }
-
-void CheckReal(char * str) {
+void CheckReal(char *str) {
   int len = strlen(str);
   int dot = 0;
   for (int i = 0; i < len; ++i) {
     if (str[i] < '0' || str[i] > '9') {
-      if(str[i] == '.') {
+      if (str[i] == '.') {
         ++dot;
-        if(dot > 1) {
+        if (dot > 1) {
           throw(0);
         }
       } else {
@@ -96,7 +96,7 @@ void CheckReal(char * str) {
 
 void CheckModify(std::string txt1) {
   std::string txt = txt1;
-  if(txt.empty()) {
+  if (txt.empty()) {
     throw(0);
   }
   bool isbn = 0;
@@ -104,158 +104,173 @@ void CheckModify(std::string txt1) {
   bool author = 0;
   bool keyword = 0;
   bool price = 0;
-  while(!txt.empty()) {
+  while (!txt.empty()) {
     std::string addtion = ProcessTxt(txt);
-    if(addtion.empty()) {
+    if (addtion.empty()) {
       throw(0);
     }
-    if(addtion[0] != '-') {
+    if (addtion[0] != '-') {
       throw(0);
     }
     addtion.erase(0, 1);
-    if(addtion.empty()) {
+    if (addtion.empty()) {
       throw(0);
     }
-     if(addtion.size() < 6) {
+    if (addtion.size() < 6) {
       throw(0);
     }
-  if(addtion[0] == 'I') {
-    if(addtion[1] != 'S') {
+    if (addtion[0] == 'I') {
+      if (addtion[1] != 'S') {
+        throw(0);
+      }
+      if (addtion[2] != 'B') {
+        throw(0);
+      }
+      if (addtion[3] != 'N') {
+        throw(0);
+      }
+      if (addtion[4] != '=') {
+        throw(0);
+      }
+      if (isbn) {
+        throw(0);
+      }
+      addtion.erase(0, 5);
+      CheckSize20(const_cast<char *>(addtion.c_str()));
+      CheckVisible(const_cast<char *>(addtion.c_str()));
+      isbn = 1;
+      continue;
+    }
+    if (addtion.size() < 7) {
       throw(0);
     }
-    if(addtion[2] != 'B') {
+    if (addtion[0] == 'p') {
+      if (addtion[1] != 'r') {
+        throw(0);
+      }
+      if (addtion[2] != 'i') {
+        throw(0);
+      }
+      if (addtion[3] != 'c') {
+        throw(0);
+      }
+      if (addtion[4] != 'e') {
+        throw(0);
+      }
+      if (addtion[5] != '=') {
+        throw(0);
+      }
+      if (price) {
+        throw(0);
+      }
+      addtion.erase(0, 6);
+      CheckReal(const_cast<char *>(addtion.c_str()));
+      price = 1;
+      continue;
+    }
+    if (addtion[addtion.size() - 1] != '\"') {
       throw(0);
     }
-    if(addtion[3] != 'N') {
+    addtion.erase(addtion.size() - 1,
+                  1); // 在剩下的所有指令中，最后一位必须是引号。
+    if (addtion.size() < 7) {
       throw(0);
     }
-    if(addtion[4] != '=') {
+    if (addtion[0] == 'n') {
+      if (addtion[1] != 'a') {
+        throw(0);
+      }
+      if (addtion[2] != 'm') {
+        throw(0);
+      }
+      if (addtion[3] != 'e') {
+        throw(0);
+      }
+      if (addtion[4] != '=') {
+        throw(0);
+      }
+      if (addtion[5] != '\"') {
+        throw(0);
+      }
+      if (name) {
+        throw(0);
+      }
+      addtion.erase(0, 6);
+      CheckSize60(const_cast<char *>(addtion.c_str()));
+      CheckVisibleNoQuotation(const_cast<char *>(addtion.c_str()));
+      name = 1;
+      continue;
+    }
+    if (addtion.size() < 9) {
       throw(0);
     }
-    if(isbn) {
+    if (addtion[0] == 'a') {
+      if (addtion[1] != 'u') {
+        throw(0);
+      }
+      if (addtion[2] != 't') {
+        throw(0);
+      }
+      if (addtion[3] != 'h') {
+        throw(0);
+      }
+      if (addtion[4] != 'o') {
+        throw(0);
+      }
+      if (addtion[5] != 'r') {
+        throw(0);
+      }
+      if (addtion[6] != '=') {
+        throw(0);
+      }
+      if (addtion[7] != '\"') {
+        throw(0);
+      }
+      if (author) {
+        throw(0);
+      }
+      addtion.erase(0, 8);
+      CheckSize60(const_cast<char *>(addtion.c_str()));
+      CheckVisibleNoQuotation(const_cast<char *>(addtion.c_str()));
+      author = 1;
+      continue;
+    }
+    if (addtion.size() < 10) {
       throw(0);
     }
-    isbn = 1;
-    continue;
-  }
-  if(addtion.size() < 7) {
-    throw(0);
-  }
-  if(addtion[0] == 'p') {
-    if(addtion[1] != 'r') {
-      throw(0);
-    }
-    if(addtion[2] != 'i') {
-      throw(0);
-    }
-    if(addtion[3] != 'c') {
-      throw(0);
-    }
-    if(addtion[4] != 'e') {
-      throw(0);
-    }
-    if(addtion[5] != '=') {
-      throw(0);
-    }
-    if(price) {
-      throw(0);
-    }
-    price = 1;
-    continue;
-  }
-  if(addtion[addtion.size() - 1] != '\"') {
-    throw(0);
-  }
-  addtion.erase(addtion.size() - 1, 1);//在剩下的所有指令中，最后一位必须是引号。
-  if(addtion.size() < 7) {
-    throw(0);
-  }
-  if(addtion[0] == 'n') {
-    if(addtion[1] != 'a') {
-      throw(0);
-    }
-    if(addtion[2] != 'm') {
-      throw(0);
-    }
-    if(addtion[3] != 'e') {
-      throw(0);
-    }
-    if(addtion[4] != '=') {
-      throw(0);
-    }
-    if(addtion[5] != '\"') {
-      throw(0);
-    }
-    if(name) {
-      throw(0);
-    }
-    name = 1;
-    continue;
-  }
-  if(addtion.size() < 9) {
-    throw(0);
-  }
-  if(addtion[0] == 'a') {
-    if(addtion[1] != 'u') {
-      throw(0);
-    }
-    if(addtion[2] != 't') {
-      throw(0);
-    }
-    if(addtion[3] != 'h') {
-      throw(0);
-    }
-    if(addtion[4] != 'o') {
-      throw(0);
-    }
-    if(addtion[5] != 'r') {
-      throw(0);
-    }
-    if(addtion[6] != '=') {
-      throw(0);
-    }
-    if(addtion[7] != '\"') {
-      throw(0);
-    }
-    if(author) {
-      throw(0);
-    }
-    author = 1;
-    continue;
-  }
-  if(addtion.size() < 10) {
-    throw(0);
-  }
-  if(addtion[0] == 'k') {
-    if(addtion[1] != 'e') {
-      throw(0);
-    }
-    if(addtion[2] != 'y') {
-      throw(0);
-    }
-    if(addtion[3] != 'w') {
-      throw(0);
-    }
-    if(addtion[4] != 'o') {
-      throw(0);
-    }
-    if(addtion[5] != 'r') {
-      throw(0);
-    }
-    if(addtion[6] != 'd') {
-      throw(0);
-    }
-    if(addtion[7] != '=') {
-      throw(0);
-    }
-    if(addtion[8] != '\"') {
-      throw(0);
-    }
-    if(keyword) {
-      throw(0);
-    }
-    keyword = 1;
-    continue;
+    if (addtion[0] == 'k') {
+      if (addtion[1] != 'e') {
+        throw(0);
+      }
+      if (addtion[2] != 'y') {
+        throw(0);
+      }
+      if (addtion[3] != 'w') {
+        throw(0);
+      }
+      if (addtion[4] != 'o') {
+        throw(0);
+      }
+      if (addtion[5] != 'r') {
+        throw(0);
+      }
+      if (addtion[6] != 'd') {
+        throw(0);
+      }
+      if (addtion[7] != '=') {
+        throw(0);
+      }
+      if (addtion[8] != '\"') {
+        throw(0);
+      }
+      if (keyword) {
+        throw(0);
+      }
+      addtion.erase(0, 9);
+      CheckSize60(const_cast<char *>(addtion.c_str()));
+      CheckVisibleNoQuotation(const_cast<char *>(addtion.c_str()));
+      keyword = 1;
+      continue;
     }
     throw(0);
   }
