6cb35a9c8490ec476c7b20a7021ccf7ffe9cf0f6
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-12 19:50:30 +0800

    fix:Acutually make a bug!

--- Rubbish_Bookstore/main.cpp
+++ Rubbish_Bookstore/main.cpp
@@ -40,6 +40,9 @@ int main() {
       getline(cin, res);
       txt = res;
       string command = ProcessTxt(txt); // 捕获第一条指令。
+      if (command == "") {
+        continue;
+      }
       if (command == "su") {
         if(!status.empty()) {
           recorder.user = status.top();
