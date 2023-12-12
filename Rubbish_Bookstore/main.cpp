d6e574265d3e01c60b034236fe1a4593126c222a
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-12 21:23:30 +0800

    fix:greater check!

--- Rubbish_Bookstore/main.cpp
+++ Rubbish_Bookstore/main.cpp
@@ -38,7 +38,13 @@ int main() {
     try {
       InitialOp(recorder);
       getline(cin, res);
+      fflush(stdin);
       txt = res;
+      for(int i = 0; i < res.size(); i++) {
+        if(res[i] > 126 || res[i] < 31) {
+          throw(0);
+        }
+      }
       string command = ProcessTxt(txt); // 捕获第一条指令。
       if (command == "") {
         continue;
