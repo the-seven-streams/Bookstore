184bab7b925b4ef60ec20ac7b5fc0b64319e392f
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-13 14:32:51 +0800

    docs:test part show.

--- Rubbish_Bookstore/main.cpp
+++ Rubbish_Bookstore/main.cpp
@@ -245,7 +245,6 @@ int main() {
         }
         CheckEmpty(txt);
         if (addtion.empty()) {
-          assert(0);
           Showall();
           recorder.user = status.top();
           strcpy(recorder.command, "show");
@@ -260,6 +259,7 @@ int main() {
         if (addtion.empty()) {
           throw(0);
         } // 弟啊，你参数呢。
+        assert(0);
         Processshow(addtion);
         recorder.user = status.top();
         strcpy(recorder.command, "show");
