1a72289681e7ad4887abb659445c764d537df878
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-13 14:01:12 +0800

    docs:test.

--- Rubbish_Bookstore/main.cpp
+++ Rubbish_Bookstore/main.cpp
@@ -267,7 +267,6 @@ int main() {
         continue;
       }
       if (command == "modify") {
-        assert(0);
         if (current_power < 3) {
           throw(0);
         }
@@ -285,6 +284,7 @@ int main() {
         continue;
       }
       if (command == "import") {
+        assert(0);
         if (current_power < 3) {
           throw(0);
         }
