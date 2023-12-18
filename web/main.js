e63e453cf988da59da98a510e51a8c0930cd2d48
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-18 13:55:38 +0800

    feat:now can communicate successfully.

--- web/main.js
+++ web/main.js
@@ -10,21 +10,18 @@ var server = http.createServer(function(request, response) {
   });
 }).listen(52273, function() {
   console.log("START");  
-  run = spawn('./code.exe');//初始化
+  run = spawn('./code');//初始化
 });
 var io = socketio.listen(server);
 io.sockets.on("connection", function(socket) {
   socket.on('clientdata', function(clientdata) {
-    var txt;
     run.stdin.write(clientdata + '\n');//接收客户端数据
     run.stdout.on('data', function(data) {
+      var txt;
       txt = data.toString();
       console.log(txt);
-      socket.emit('serverdata', txt);//发送数据给客户端
+      io.emit('serverData', txt);//发送数据给客户端
       run.stdout.removeAllListeners('data');
     })
   })
-});
-io.sockets.on('serverdata', function(data) {
-  console.log(data);
 });
