f6929e230c1cbd40d6d84bd9045343f351bbd67f
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2023-12-18 11:18:43 +0800

    feat:finish the basic communication from client to server.

--- web/main.js
+++ web/main.js
@@ -1,22 +1,25 @@
-const https = require("https");
-const fs = require("fs");
-const spawn = require('child_process');
-const socketio = require("socket.io");
-var server = https.createServer(function(request, response) {
+var http = require("http");
+var fs = require("fs");
+var {spawn} = require('child_process');
+var socketio = require("socket.io");
+var run = spawn('./code');//初始化
+var server = http.createServer(function(request, response) {
   fs.readFile("main.html", function(error, data) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(data);
   });
 }).listen(52273, function() {
   console.log("START");  
+  run = spawn('./code');//初始化
 });
 var io = socketio.listen(server);
 io.sockets.on("connection", function(socket) {
-  run = spawn('./code');//初始化
+  console.log("CONNECTED!")
   run.stdout.on('Data', function(Data) {
     socket.emit('serverdata', {text: Data.toString()});//向客户端发送数据
   })
   socket.on('clientdata', function(data) {
-    spawn.stdin.write(data.text);//向子进程发送数据
+    console.log('RECEIVED DATA');
+    run.stdin.write(data);//接收客户端数据
   })
 });
